9df5275a04992ee4971c3f43845de9817738aed3
renaudrenaud <renaud.renaud@gmail.com>
2023-01-13 15:59:16 +0800

    fix: few corrections

--- lcd_cpu_2004.py
+++ lcd_cpu_2004.py
@@ -3,6 +3,7 @@ Using I2C LCD 20*04 to show information
 Initially for the Orange Pi 5
 RC 2023-01-04
 
+2023-01-07 v0.4.1: Some few corrections
 2023-01-07 v0.4.0: Orange Pi 5 and Orange Pi Zero 2 ok
 2023-01-06 v0.3.0: seems ok for Orange Pi 5
     cpuonly with cores information   
@@ -38,7 +39,7 @@ class LCD20CPU:
     """
     def __init__(self): 
         
-        self.__version__ = "v0.4.0"
+        self.__version__ = "v0.4.2"
         description = "LCD20CPU Monitor you Pi with a 20x4 LCD"
         lcd_help = "LCD address something like 0x3f"
         i2c_help = "i2cdetect port, 0 or 1, 0 for Orange Pi Zero, 1 for Rasp > V2 or OPi5"
